ceb82ce785a64fb4a87c4cb21236eb37328b8bad
David Hybner <dah1998@yahoo.com>
2022-11-21 20:31:09 -0500

    gettting gh to work
    added more to the package.json file to get github to work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Contact() {
     return(
         <section className="about" id="about-me">
-        <h1 >Contact</h1>
+        <h1 >Contacts</h1>
       
         <p className="about--paragraph">
        
