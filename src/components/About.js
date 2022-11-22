870f84dc03f0fe3e5354e26b5c8a92a6844d0113
David Hybner <dah1998@yahoo.com>
2022-11-21 21:07:15 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import nuke from '../images/nuke.png';
 export default function About() {
     return (
       <section className="about" id="about-me">
-        <h1 >About Me</h1>
+        <h1 >About Me: </h1>
       <img alt='nuke' src={nuke}/>
         <p className="about--paragraph">
        
