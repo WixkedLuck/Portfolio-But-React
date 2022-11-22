e86860f3afd6e28033992b6091277dd75d735e18
David Hybner <dah1998@yahoo.com>
2022-11-22 01:02:04 -0500

    update about component
    added images for this page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import nuke from '../images/nuke.png';
+import {FaGem} from "react-icons/fa";
+ import nuke from '../images/second.png';
+import '../styles/About.css';
 export default function About() {
+  const style = {  maxWidth: "300px" }
     return (
-      <section className="about" id="about-me">
-        <h1 >About Me: </h1>
-      <img alt='nuke' src={nuke}/>
+      
+      <section className="about App" id="about-me">
+        <h1 className='Title'>About Me: </h1>
+        <FaGem style={style}/>
+       <img className='image' alt='nuke' src={nuke} style={style}/> 
         <p className="about--paragraph">
        
         </p>
