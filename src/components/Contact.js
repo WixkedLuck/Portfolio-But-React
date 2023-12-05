54d42d4e1e40d5b2181ab0809943c2adaa138a0f
David-Hybner <dah1998@yahoo.com>
2023-12-05 14:01:33 -0500

    link email
    linked email js now form works

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '../styles/Contact.css';
 import {FaGithubAlt} from "react-icons/fa";
 import {FaLinkedinIn} from "react-icons/fa";
 import {FaCodepen} from "react-icons/fa";
-function Contact() {
-    return(
-        <section className="about" id="about-me">
-         <form id="survey-form">
-        <h1 id="title">Contact Me:</h1>
-    <p id="description"> Thank you for taking the time to reach out!</p>
-        <label for="name" id="name-label">Name:</label>
-        <input type="text" id="name" required placeholder="Enter your name"></input> 
-        <label for="email" id="email-label">Email:</label>
-        <input type="email" id="email" required placeholder="Enter your email"></input> 
-        <p id="description"> While you're at it why not follow me? </p>
-        <a className='Icons' title="GitHub" href="https://github.com/WixkedLuck/" target="_blank" rel="noreferrer"><FaGithubAlt size={40}/></a> 
-       <a className='Icons' title="LinkedIn" href="https://www.linkedin.com/in/david-a-hybner/" target="_blank" rel="noreferrer"><FaLinkedinIn size={40}/></a> 
-       <a className='Icons' title="Codepen" href="https://codepen.io/WixkedLuck" target="_blank" rel="noreferrer" ><FaCodepen size={40}/></a> 
-        <p>Additional Comments here: </p>
-        <textarea id="comments" placeholder='Awesome Portfolio!'></textarea>
-        <button type="submit" id="submit">Submit</button>
-       
-    </form>
-      
-   
-      </section> 
+import emailjs from '@emailjs/browser';
+
+export default function ContactUs  () {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_tipirs6', 'template_316rbxk', form.current, 'FDfP_izbBnC__vuhL')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
+    return (
+        
+      <form ref={form} onSubmit={sendEmail} className='padding-round'>
+        <p>Thank you for taking the time to reach out to me and viewing my portfolio! I look forward to speaking with you!</p>
+        <p>Please fill out this form below and I will get back to you as soon as I can!</p>
+        <label>Name:</label>
+        <input type="text" name="user_name" className='spacing' />
+        <label>Email:</label>
+        <input type="email" name="user_email" className='spacing'/><br></br>
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     );
-}
-export default Contact;
+  };
